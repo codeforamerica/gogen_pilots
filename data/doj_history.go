e8989a32a92263417142dc5b7d7926a1097b847b
Laura Kogler <lkogler@codeforamerica.org>
2018-11-30 17:44:30 -0800

    wip

--- data/doj_history.go
+++ data/doj_history.go
@@ -23,11 +23,30 @@ func (history DOJHistory) PushRow(row DOJRow) {
 		history.CDL = row.CDL
 	}
 
-	if(row.Convicted) {
+	if (row.Convicted) {
 		history.Convictions = append(history.Convictions, row)
 	}
 
-	if(row.PC290Registration) {
+	if (row.PC290Registration) {
 		history.PC290Registration = true
+	}
+}
+
+func (history *DOJHistory) match(entry CMSEntry) MatchData {
+	var results = make(map[string]bool)
+	results["ssn"] = entry.SSN != "" && entry.SSN == history.SSN
+	results["cdl"] = entry.CDL != "" && entry.CDL == history.CDL
+
+	matchStrength := 0
+	for _, val := range results {
+		if val {
+			matchStrength++
+		}
+	}
+
+	return MatchData{
+		history:       history,
+		matchResults:  results,
+		matchStrength: matchStrength,
 	}
 }
--- data/doj_history_test.go
+++ data/doj_history_test.go
@@ -2,22 +2,39 @@ package data_test
 
 import (
 	"gogen/data"
+	"time"
 
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("DOJHistory", func() {
-	Describe("PushRow", func() {
+
+
+	Describe("match", func() {
 		Context("An empty history", func() {
 			var history data.DOJHistory
 
 			BeforeEach(func() {
+				rows := []data.DOJRow{
+					{SubjectID: "subj_id", Name: "soup, zak e", CII: "12345678", SSN: "345678125", OFN: "1234", DOB: time.Time{}, CodeSection: "11357 HS", Convicted: true, DispositionDate: time.Date(2008,time.May,4, 0,0,0,0,nil)},
+					{SubjectID: "subj_id", Name: "soup, zak e", CII: "12345678", SSN: "345678125", OFN: "1235", DOB: time.Time{}, CodeSection: "11357 HS", Convicted: false, DispositionDate: time.Date(2008,time.May,4, 0,0,0,0,nil) },
+					{SubjectID: "subj_id", Name: "soup, zak e", CII: "12345678", SSN: "345678125", OFN: "1119999", DOB: time.Time{}, CodeSection: "11360 HS", Convicted: true, DispositionDate: time.Date(2008,time.May,4, 0,0,0,0,nil), County: "LOS ANGELES" },
+					{SubjectID: "subj_id", Name: "soup, zak e", CII: "12345678", SSN: "345678125", OFN: "1236 12345678-00", DOB: time.Time{}, CodeSection: "11360 HS", Convicted: true, DispositionDate: time.Date(2008,time.May,4, 0,0,0,0,nil), County: "SAN FRANCISCO" },
+					{SubjectID: "subj_id", Name: "soup, zak e", CII: "12345678", SSN: "345678125", OFN: "1236 12345678-00", DOB: time.Time{}, CodeSection: "11360 HS", Convicted: true, DispositionDate: time.Date(2008,time.May,4, 0,0,0,0,nil) },
+				}
 				history = data.DOJHistory{}
+				for _, row := range rows {
+					history.PushRow(row)
+				}
 			})
 
-			PIt("Sets the name of the history", func() {
-				
+			PIt("Sets the correct values on the history", func() {
+				Expect(history.SubjectID).To(Equal("subj_id"))
+				Expect(history.Name).To(Equal("soup, zak e"))
+				Expect(history.CII).To(Equal("12345678"))
+				Expect(history.SSN).To(Equal("345678125"))
+				Expect(history.DOB).To(Equal(time.Time{}))
 			})
 		})
 	})
