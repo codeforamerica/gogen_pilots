994f11c9f4fb7504b515683ab90f1fbcef868ce3
Laura Kogler <lkogler@codeforamerica.org>
2019-05-02 13:57:34 -0700

    Create unique personal info for each person in generated file
    [#165693700]
    
    Co-authored-by: Kristi Lanzisera <kristi@codeforamerica.org>

--- generate_test_data/generate_test_data.go
+++ generate_test_data/generate_test_data.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jessevdk/go-flags"
 	. "gogen/data"
 	. "gogen/processor"
+	"math"
 	"math/rand"
 	"path/filepath"
+	"strconv"
+	"time"
 )
 
 var opts struct {
 	OutputFolder string `long:"outputs" description:"The folder in which to place result files" required:"true"`
 	TargetSize   int    `long:"target-size" description:"Desired number of lines in the output file" required:"true"`
-	County		 string `long:"county" description:"Desired county" required:"true"`
+	County       string `long:"county" description:"Desired county" required:"true"`
+}
+
+var counters struct {
+	SubjectId int
 }
 
 func main() {
@@ -24,6 +32,7 @@ func main() {
 
 	totalRows := 0
 
+	counters.SubjectId = 10000000
 	for totalRows < opts.TargetSize {
 		rows := generateHistory()
 		totalRows += len(rows)
@@ -84,14 +93,40 @@ func generateProp64Row(infos PersonalInfos) []string {
 }
 
 func generatePersonalInfos() PersonalInfos {
+	counters.SubjectId++
+
 	return PersonalInfos{
-		SubjectId: "10",
-		CII: "cii",
-		Name: "SMITH,JOHN",
-		DOB: "19790620",
-		SSN: "123456789",
-		CDL: "B6320998",
+		SubjectId: strconv.Itoa(counters.SubjectId),
+		CII:       randomNDigitNumber(10),
+		Name:      randomChoice(lastNames) + "," + randomChoice(firstNames),
+		DOB:       randomDate(time.Date(1950, 1, 0, 0, 0, 0, 0, time.UTC), time.Date(2001, 1, 0, 0, 0, 0, 0, time.UTC)),
+		SSN:       randomNDigitNumber(9),
+		CDL:       randomLetter() + randomNDigitNumber(7),
 	}
+}
+
+func randomDate(startDate time.Time, endDate time.Time) string {
+	min := startDate.Unix()
+	max := endDate.Unix()
+	delta := max - min
+
+	randomDateInSeconds := rand.Int63n(delta) + min
+	randomDate := time.Unix(randomDateInSeconds, 0)
+	return randomDate.Format("20060102")
+}
+
+func randomLetter() string {
+	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	return string(alphabet[rand.Intn(len(alphabet))])
+}
+
+func randomNDigitNumber(n int) string {
+	formatString := fmt.Sprintf("%%0%dd", n)
+	return fmt.Sprintf(formatString, rand.Intn(int(math.Pow10(n))))
+}
+
+func randomChoice(values []string) string {
+	return values[rand.Intn(len(values))]
 }
 
 type PersonalInfos struct {
@@ -101,4 +136,200 @@ type PersonalInfos struct {
 	DOB       string
 	SSN       string
 	CDL       string
+}
+
+var firstNames = []string{
+	"MICHAEL",
+	"JENNIFER",
+	"CHRISTOPHER",
+	"AMY",
+	"JASON",
+	"MELISSA",
+	"DAVID",
+	"MICHELLE",
+	"JAMES",
+	"KIMBERLY",
+	"JOHN",
+	"LISA",
+	"ROBERT",
+	"ANGELA",
+	"BRIAN",
+	"HEATHER",
+	"WILLIAM",
+	"STEPHANIE",
+	"MATTHEW",
+	"NICOLE",
+	"JOSEPH",
+	"JESSICA",
+	"DANIEL",
+	"ELIZABETH",
+	"KEVIN",
+	"REBECCA",
+	"ERIC",
+	"KELLY",
+	"JEFFREY",
+	"MARY",
+	"RICHARD",
+	"CHRISTINA",
+	"SCOTT",
+	"AMANDA",
+	"MARK",
+	"JULIE",
+	"STEVEN",
+	"SARAH",
+	"THOMAS",
+	"LAURA",
+	"TIMOTHY",
+	"SHANNON",
+	"ANTHONY",
+	"CHRISTINE",
+	"CHARLES",
+	"TAMMY",
+	"JOSHUA",
+	"TRACY",
+	"RYAN",
+	"KAREN",
+	"JEREMY",
+	"DAWN",
+	"PAUL",
+	"SUSAN",
+	"ANDREW",
+	"ANDREA",
+	"IAN",
+	"MACKENZIE",
+	"JASON",
+	"KHLOE",
+	"AYDEN",
+	"SOPHIE",
+	"ADAM",
+	"KATHERINE",
+	"PARKER",
+	"PAISLEY",
+	"COOPER",
+	"MILA",
+	"JUSTIN",
+	"EVA",
+	"XAVIER",
+	"NAOMI",
+	"NOLAN",
+	"ELEANOR",
+	"JACE",
+	"GIANNA",
+	"HUDSON",
+	"MELANIE",
+	"CARSON",
+	"AUBREE",
+	"BENTLEY",
+	"FAITH",
+	"LINCOLN",
+	"KAYLA",
+	"BLAKE",
+	"PIPER",
+	"EASTON",
+	"MADELINE",
+	"NATHANIEL",
+	"LYDIA",
+}
+
+var lastNames = []string{
+	"SMITH",
+	"JOHNSON",
+	"WILLIAMS",
+	"JONES",
+	"BROWN",
+	"DAVIS",
+	"MILLER",
+	"WILSON",
+	"MOORE",
+	"TAYLOR",
+	"ANDERSON",
+	"THOMAS",
+	"JACKSON",
+	"WHITE",
+	"HARRIS",
+	"MARTIN",
+	"THOMPSON",
+	"GARCIA",
+	"MARTINEZ",
+	"ROBINSON",
+	"CLARK",
+	"RODRIGUEZ",
+	"LEWIS",
+	"LEE",
+	"WALKER",
+	"HALL",
+	"ALLEN",
+	"YOUNG",
+	"HERNANDEZ",
+	"KING",
+	"WRIGHT",
+	"LOPEZ",
+	"HILL",
+	"SCOTT",
+	"GREEN",
+	"ADAMS",
+	"BAKER",
+	"GONZALEZ",
+	"NELSON",
+	"CARTER",
+	"MITCHELL",
+	"PEREZ",
+	"ROBERTS",
+	"TURNER",
+	"PHILLIPS",
+	"CAMPBELL",
+	"PARKER",
+	"EVANS",
+	"EDWARDS",
+	"COLLINS",
+	"STEWART",
+	"SANCHEZ",
+	"MORRIS",
+	"ROGERS",
+	"REED",
+	"COOK",
+	"MORGAN",
+	"BELL",
+	"MURPHY",
+	"BAILEY",
+	"RIVERA",
+	"COOPER",
+	"RICHARDSON",
+	"COX",
+	"HOWARD",
+	"WARD",
+	"TORRES",
+	"PETERSON",
+	"GRAY",
+	"RAMIREZ",
+	"JAMES",
+	"WATSON",
+	"BROOKS",
+	"KELLY",
+	"SANDERS",
+	"PRICE",
+	"BENNETT",
+	"WOOD",
+	"BARNES",
+	"ROSS",
+	"HENDERSON",
+	"COLEMAN",
+	"JENKINS",
+	"PERRY",
+	"POWELL",
+	"LONG",
+	"PATTERSON",
+	"HUGHES",
+	"FLORES",
+	"WASHINGTON",
+	"BUTLER",
+	"SIMMONS",
+	"FOSTER",
+	"GONZALES",
+	"BRYANT",
+	"ALEXANDER",
+	"RUSSELL",
+	"GRIFFIN",
+	"DIAZ",
+	"HAYES ",
 }
