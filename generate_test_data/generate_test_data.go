678f7e4e2810945873e569a732a137f980fdfba8
Laura Kogler <lkogler@codeforamerica.org>
2019-05-02 11:29:49 -0700

    Generate variable number of P64 convictions per history
    * also extract method to generate personal info
    
    [#165693700]
    
    Co-authored-by: Kristi Lanzisera <kristi@codeforamerica.org>

--- generate_test_data/generate_test_data.go
+++ generate_test_data/generate_test_data.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/jessevdk/go-flags"
-	. "gogen/processor"
 	. "gogen/data"
+	. "gogen/processor"
+	"math/rand"
 	"path/filepath"
 )
 
 var opts struct {
 	OutputFolder string `long:"outputs" description:"The folder in which to place result files" required:"true"`
 	TargetSize   int    `long:"target-size" description:"Desired number of lines in the output file" required:"true"`
+	County		 string `long:"county" description:"Desired county" required:"true"`
 }
 
 func main() {
@@ -35,22 +37,42 @@ func main() {
 }
 
 func generateHistory() [][]string {
+	var rows [][]string
+
+	personalInfos := generatePersonalInfos()
+
+	randomNumber := rand.Float64()
+	numberOfProp64Convictions := 1
+	if randomNumber > 0.95 {
+		numberOfProp64Convictions = 3
+	} else if randomNumber > 0.8 {
+		numberOfProp64Convictions = 2
+	}
+
+	for i := 0; i < numberOfProp64Convictions; i++ {
+		rows = append(rows, generateProp64Row(personalInfos))
+	}
+
+	return rows
+}
+
+func generateProp64Row(infos PersonalInfos) []string {
 	numberOfColumns := len(DojFullHeaders)
 	row := make([]string, numberOfColumns)
 
-	row[SUBJECT_ID] = "10"
-	row[CII_NUMBER] = "cii"
-	row[PRI_NAME] = "SMITH,JOHN"
-	row[PRI_DOB] = "19790620"
-	row[PRI_SSN] = "123456789"
-	row[PRI_CDL] = "B6320998"
+	row[SUBJECT_ID] = infos.SubjectId
+	row[CII_NUMBER] = infos.CII
+	row[PRI_NAME] = infos.Name
+	row[PRI_DOB] = infos.DOB
+	row[PRI_SSN] = infos.SSN
+	row[PRI_CDL] = infos.CDL
 	row[CYC_DATE] = "20040424"
 	row[STP_EVENT_DATE] = "20050621"
 	row[STP_TYPE_DESCR] = "COURT"
-	row[STP_ORI_CNTY_NAME] = "ALAMEDA"
+	row[STP_ORI_CNTY_NAME] = opts.County
 	row[CNT_ORDER] = "001002004000"
 	row[OFN] = "1234K5n3"
-	row[OFFENSE_DESCR] = "POSSESSION OF MARIJUANA"
+	row[OFFENSE_DESCR] = "11357 HS-POSSESSION OF MARIJUANA"
 	row[OFFENSE_TOC] = "F"
 	row[FE_NUM_CRT_CASE] = "2342H8a8J"
 	row[DISP_DESCR] = "CONVICTED"
@@ -58,6 +80,25 @@ func generateHistory() [][]string {
 	row[SENT_LENGTH] = "45"
 	row[SENT_TIME_CODE] = "D"
 	row[COMMENT_TEXT] = "THIS IS A COMMENT"
+	return row
+}
 
-	return [][]string{row}
+func generatePersonalInfos() PersonalInfos {
+	return PersonalInfos{
+		SubjectId: "10",
+		CII: "cii",
+		Name: "SMITH,JOHN",
+		DOB: "19790620",
+		SSN: "123456789",
+		CDL: "B6320998",
+	}
+}
+
+type PersonalInfos struct {
+	SubjectId string
+	CII       string
+	Name      string
+	DOB       string
+	SSN       string
+	CDL       string
 }
