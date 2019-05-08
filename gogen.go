05cd44a3961b4a63be73ad67337676acc60fcf0b
Laura Kogler <lkogler@codeforamerica.org>
2019-05-08 14:54:57 -0700

    Add deployment of compiled binaries to github releases
    [#165161530]
    
    Co-authored-by: Vraj Mohan <vraj@codeforamerica.org>
    Co-authored-by: Zak Auerbach <zak@codeforamerica.org>

--- gogen.go
+++ gogen.go
@@ -1,25 +1,45 @@
 package main
 
 import (
+	"fmt"
 	. "gogen/data"
 	. "gogen/processor"
+	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/jessevdk/go-flags"
 )
 
+const VERSION = "0.0.1"
+
+var defaultOpts struct {}
+
 var opts struct {
-	OutputFolder string `long:"outputs" description:"The folder in which to place result files" required:"true"`
-	DOJFile      string `long:"input-doj" description:"The file containing criminal histories from CA DOJ" required:"true"`
+	OutputFolder string `long:"outputs" description:"The folder in which to place result files"`
+	DOJFile      string `long:"input-doj" description:"The file containing criminal histories from CA DOJ"`
 	Delimiter    string `long:"delimiter" short:"d" default:"," hidden:"true"`
-	County       string `long:"county" short:"c" description:"The county for which eligibility will be computed" required:"true"`
+	County       string `long:"county" short:"c" description:"The county for which eligibility will be computed"`
+	Version      bool `long:"version" short:"v" description:"Print the version"`
 }
 
 func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
-		panic(err)
+		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		} else {
+			os.Exit(1)
+		}
+	}
+
+	if opts.Version {
+		fmt.Println(VERSION)
+		os.Exit(0)
+	}
+
+	if opts.OutputFolder == "" || opts.DOJFile == "" || opts.County == "" {
+		panic("Missing required field! Run gogen --help for more info.")
 	}
 
 	dojInformation, _ := NewDOJInformation(opts.DOJFile, time.Now(), opts.County)
