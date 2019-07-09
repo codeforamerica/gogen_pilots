ce056335bf5d8d9b422c3484fdb05c9c412fef41
Vraj Mohan <vraj@codeforamerica.org>
2019-07-09 16:55:54 -0700

    Remove extraneous pattern from regex
    [#164714666]

--- matchers/code_section_matchers.go
+++ matchers/code_section_matchers.go
@@ -4,8 +4,8 @@ import (
 	"regexp"
 )
 
-var prop64matcher = regexp.MustCompile(`(11357|11358|11359|11360).*`)
-var relatedChargeMatcher = regexp.MustCompile(`(647\(f\)\s*PC|602\s*PC|466\s*PC|148\.9\s*PC|148\s*PC|11364\s*HS|11550\s*HS|4140\s*BP|4149\s*BP|4060\s*BP|40508\s*VC|1320[^\d\.][^\.]*PC).*`)
+var prop64matcher = regexp.MustCompile(`(11357|11358|11359|11360)`)
+var relatedChargeMatcher = regexp.MustCompile(`(647\(f\)\s*PC|602\s*PC|466\s*PC|148\.9\s*PC|148\s*PC|11364\s*HS|11550\s*HS|4140\s*BP|4149\s*BP|4060\s*BP|40508\s*VC|1320[^\d\.][^\.]*PC)`)
 
 func ExtractProp64Section(codeSection string) (bool, string) {
 	if IsProp64Charge(codeSection) {
