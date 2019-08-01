3c2c21261151b47a144cc747708f7b0fe85633d2
Vraj Mohan <vraj@codeforamerica.org>
2019-08-01 10:20:54 -0700

    Match only code section number and sub-sections
    The HS designation is not consistently present in the DOJ extracts.
    We have seen 11359 encoded incorrectly as "11359 PC".
    
    Also added a test for "attempted" charges where a 664 prefix is used.
    
    [#167404910]

--- matchers/code_section_matchers.go
+++ matchers/code_section_matchers.go
@@ -7,14 +7,14 @@ import (
 var prop64matcher = regexp.MustCompile(`(11357|11358|11359|11360)`)
 var relatedChargeMatcher = regexp.MustCompile(`(647\(f\)\s*PC|602\s*PC|466\s*PC|148\.9\s*PC|148\s*PC|11364\s*HS|11550\s*HS|4140\s*BP|4149\s*BP|4060\s*BP|40508\s*VC|1320[^\d\.][^\.]*PC)`)
 var Prop64MatchersByCodeSection = map[string]*regexp.Regexp{
-	"11357(a)":              regexp.MustCompile(`11357\(A\).*HS`),
-	"11357(b)":              regexp.MustCompile(`11357\(B\).*HS`),
-	"11357(c)":              regexp.MustCompile(`11357\(C\).*HS`),
-	"11357(d)":              regexp.MustCompile(`11357\(D\).*HS`),
-	"11357(no-sub-section)": regexp.MustCompile(`11357(\([^A-D]\)|[^(]).*HS`),
-	"11358":                 regexp.MustCompile(`11358.*HS`),
-	"11359":                 regexp.MustCompile(`11359.*HS`),
-	"11360":                 regexp.MustCompile(`11360.*HS`),
+	"11357(a)":              regexp.MustCompile(`11357\(A\).*`),
+	"11357(b)":              regexp.MustCompile(`11357\(B\).*`),
+	"11357(c)":              regexp.MustCompile(`11357\(C\).*`),
+	"11357(d)":              regexp.MustCompile(`11357\(D\).*`),
+	"11357(no-sub-section)": regexp.MustCompile(`11357(\([^A-D]\)|[^(]).*`),
+	"11358":                 regexp.MustCompile(`11358.*`),
+	"11359":                 regexp.MustCompile(`11359.*`),
+	"11360":                 regexp.MustCompile(`11360.*`),
 }
 
 func ExtractProp64Section(codeSection string) (bool, string) {
